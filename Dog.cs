31760a5d1ecf3e7e9c4244bcf732d1ca21d06986
Da-Boi42 <147948548+Da-Boi42@users.noreply.github.com>
2023-11-09 13:52:11 +0200

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mine
+{
+    class Program
+    {
+        class Dog
+        {
+        public string name;
+        public int age;
+
+        
+        }
+        static void Main(string[] args)
+        {
+        Dog bob = new Dog();
+        bob.name = "Bobby";
+        bob.age = 5;
+
+        Console.WriteLine(bob.age);
+        Console.WriteLine(bob.name);
+        }
+    }
+}
